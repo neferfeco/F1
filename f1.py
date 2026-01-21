a4899837e627149f21d2950f666eab2c9d6f8c31
Mona Lisa <fpiller@gmail.com>
2026-01-21 14:06:29 +0100

    1

--- f1.py
+++ f1.py
@@ -17,17 +17,33 @@ except IOError as ex:
 
 #print(verseny_adatok)
 
-versenyzok = []
+versenyzo_adatok = []
 
-for sor in verseny_adatok:
-    adatok = []
-    if (sor[2],sor[1]) not in versenyzok.:
-        adatok.append(sor[2])
-        adatok.append(sor[1])
+elso = [verseny_adatok[0][2], verseny_adatok[0][1]]
+versenyzo_adatok.append(elso)
+
+for i in range(1, len(verseny_adatok)):
+    j = 0
+    while j < len(versenyzo_adatok) and versenyzo_adatok[j][0] != verseny_adatok[i][2]:
+        j += 1
+
+    if j >= len(versenyzo_adatok):
+        uj = [verseny_adatok[i][2], verseny_adatok[i][1]]
+        versenyzo_adatok.append(uj)
     else:
-        pass    
-        
-    versenyzok.append(adatok)
+        if versenyzo_adatok[j][1] > verseny_adatok[i][1]:
+            versenyzo_adatok[j][1] = verseny_adatok[i][1]
 
-print(versenyzok)
+#print(versenyzo_adatok)
+
+i = 0
+while i < len(versenyzo_adatok) and versenyzo_adatok[i][1] == 1:
+    i +=1
+
+if i >= len(versenyzo_adatok):
+    print(f"Minden versenyzo volt elso helyezett!")
+else:
+    print(f"Nem minden versenyzo volt elso helyezett! Pl.: {versenyzo_adatok[i][0]}")
+
+
 
